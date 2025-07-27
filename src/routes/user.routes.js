551a5fde0baab709c834ef398038839843cc8d69
mandara-samanalee <110854878+mandara-samanalee@users.noreply.github.com>
2025-07-27 14:08:07 +0530

    fix swagger issue

---
+++ src/routes/user.routes.js
@@ -0,0 +1,75 @@
+import { forgotPassword, changePasswordController } from '../controllers/user.controller.js';
+import express from 'express';
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /user/forgot-password:
+ *   post:
+ *     summary: Reset user password
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               newPassword:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Password reset successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/user/forgot-password', forgotPassword);
+
+/**
+ * @swagger
+ * /user/change-password/{userId}:
+ *   post:
+ *     summary: Change user password
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               currentPassword:
+ *                 type: string
+ *               newPassword:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Password changed successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/user/change-password/:userId', changePasswordController);
+
+export default router;
