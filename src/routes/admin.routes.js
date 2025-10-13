c2f62d200d8cb7793fc952d13fbdcc6d0581b30c
mandara-samanalee <110854878+mandara-samanalee@users.noreply.github.com>
2025-10-14 01:41:18 +0530

    test github actions

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -14,14 +14,19 @@ const router = express.Router();
 
 router.put("/update-profile/:userId", upload.single('image'), updateAdminProfileController);
 router.get("/getdetails/:userId", getAdminProfileController);
+
 // get customer, vendor, booking, event. services count
 router.get("/summary/counts", getDashboardSummaryController);
+
 // get newly registered customers and vendors in past three months
 router.get("/monthly-registrations", getRecentRegistrationsController);
+
 // get the service count under each service category
 router.get("/service-counts", getServiceCountsByCategoryController);
+
 // get the most booked services
 router.get('/top-booked-services', getTopBookedServicesController);
+
 // get the top rated services
 router.get('/top-rated-services', getTopRatedServicesController);
 
